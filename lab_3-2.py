42836ab7126caeb96132783717f122db5317702d
P24aelsheikh <144148097+P24aelsheikh@users.noreply.github.com>
2023-09-26 10:56:35 -0400

    Add files via upload

---
+++ lab_3-2.py
@@ -0,0 +1,29 @@
+"""
+Create a Python file named lab_3-2.py
+Copy the following examples into a Python file one at a time. 
+Think about what kind of error you think it will throw before running the code. 
+When you have run one line, be sure to comment it before adding your next line.
+After you have commented out a line, add a comment listing what error type occured.
+	
+ 	int(a)
+	int('a')
+	'a' + 2
+	import date
+	print("I'm a happy camper!)
+
+"""
+int(a)
+#name error
+#define a
+int('a')
+#value error
+#remove parentices so code isnt seen as a comment
+'a' + 2
+#type error
+#has to be a string 
+import date
+#module not Found error
+#create a module for date
+print("I'm a happy camper!)
+#syntax error 
+#add quotation to close it off   
